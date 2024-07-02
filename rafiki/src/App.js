f4df2d214d14f860f4844bb49a0032a936e449dc
stephanie-niva <stephanienivaa@example.com>
2024-07-02 18:00:29 +0300

    add learnwithus

--- rafiki/src/App.js
+++ rafiki/src/App.js
@@ -1,22 +1,11 @@
-import './App.css';
+import LearnWithUs from "./LearnWithUs";
 
-const LearnWithUs = ()=>{
+const App = ()=>{
   return(
-      <div>
-          <h1 className="heading">
-              Learn With Us 
-          </h1 >
-          <section className="learnWithUs">
-           <img src="/Images/students.png"alt="girl studying" className="image"></img>
-           <section className="text">
-             <p>Learning students are happy students.</p>
-             <p>Happy students make happy parents.</p>
-             <p>We give parents a hand while</p>
-             <p>giving students the tools to succeed. </p>
-           </section>
-           
-          </section>
-      </div>
+    <div>
+     <LearnWithUs/>
+      
+    </div>
   )
 }
-export default LearnWithUs;
+export default App;
---
+++ rafiki/src/LearnWithUs/index.js
@@ -0,0 +1,21 @@
+import './index.css'
+const LearnWithUs = ()=>{
+    return(
+        <div>
+            <h1 className="heading">
+                Learn With Us 
+            </h1 >
+            <section className="learnWithUs">
+             <img src='Images/happystudents.jpg' alt="studying" className="image"></img>
+             <section className="text">
+               <p>Learning students are happy students.
+               Happy students make happy parents.
+               We give parents a hand while
+               giving students the tools to succeed. </p>
+             </section>
+             
+            </section>
+        </div>
+    )
+  }
+  export default LearnWithUs;

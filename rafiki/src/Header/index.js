5d62087360971b95a70d7825ee489ee146c5f69a
Loice-Staice <staiceloicea@gmail.com>
2024-07-02 17:45:55 +0300

    add files

--- rafiki/src/Header/index.js
+++ rafiki/src/Header/index.js
@@ -1,30 +1,28 @@
 import './index.css';
 
-const Header = () => {
+const About = () => {
   return(
-    <div className='header'>
+    <div className='about'>
     <div>
-    <img src="/PHOTOS/laptop.png" alt="laptop-with-video-icon" />
-    <p>
-      <p className='watch-the-lesson'>Watch the lesson</p><br />
+    <img src="/photos/laptop.png" alt="laptop-with-video-icon" />
+    <p className="watch-video">
+      <p className='watch-the-lesson'>Watch the lesson</p>
       Choose from more than 1000 <br/>videos in over 10 subjects
     </p>
   </div>
   <div>
-    <img src="/PHOTOS/laptop2.png" alt='
-    
-    -098  ' />
-    <p>
-    <p className='take-the-quiz'>Take the quiz</p><br/>Ace your math test by<br/>practicing quizzes in the app
+    <img src="/photos/quiz.png" alt=' ' />
+    <p className="take-quiz">
+    <p className='take-the-quiz'>Take the quiz</p>Ace your math test by<br/>practicing quizzes in the app
     </p>
   </div>
   <div>
-    <img src="/PHOTOS/laptop3.png" alt=''/>
-    <p>
-    <p className='ask-questions'>Ask Questions</p><br/>Get answered instantly
+    <img src="/photos/quizz.png" alt=''/>
+    <p className="ask-quiz">
+    <p className='ask-questions'>Ask Questions</p>Get answered instantly
     </p>
   </div>
   </div>
   )
 }
-export default Header;
+export default About;                                                                                                                                                                                          

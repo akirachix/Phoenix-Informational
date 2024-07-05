6e4bac22d21bfff6ba388e6d709051d9e51e775e
stephanie-niva <stephanienivaa@example.com>
2024-07-05 16:11:36 +0300

    add changes

--- rafiki/src/index.js
+++ rafiki/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import LearnWithUs from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LearnWithUs />
+    <App />
   </React.StrictMode>
 );
 

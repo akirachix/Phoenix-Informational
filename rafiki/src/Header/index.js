ede4c318657aeffad5608db2f2bac2f20ffe90b8
Loice-Staice <staiceloicea@gmail.com>
2024-07-25 11:10:00 +0300

    made changes

---
+++ rafiki/src/Header/index.js
@@ -0,0 +1,30 @@
+import './index.css';
+
+const Header = () => {
+  return(
+    <div className='header'>
+    <div>
+    <img src="/PHOTOS/laptop.png" alt="laptop-with-video-icon" />
+    <p>
+      <p className='watch-the-lesson'>Watch the lesson</p><br />
+      Choose from more than 1000 <br/>videos in over 10 subjects
+    </p>
+  </div>
+  <div>
+    <img src="/PHOTOS/laptop2.png" alt='
+    
+    -098  ' />
+    <p>
+    <p className='take-the-quiz'>Take the quiz</p><br/>Ace your math test by<br/>practicing quizzes in the app
+    </p>
+  </div>
+  <div>
+    <img src="/PHOTOS/laptop3.png" alt=''/>
+    <p>
+    <p className='ask-questions'>Ask Questions</p><br/>Get answered instantly
+    </p>
+  </div>
+  </div>
+  )
+}
+export default Header;
